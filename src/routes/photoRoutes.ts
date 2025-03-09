8627c20d4abff9d9badca1e4b95604cc7925d291
avelando <avelarrodrigues89@gmail.com>
2025-03-08 22:48:27 -0300

    Correções na rota de upload

--- src/routes/photoRoutes.ts
+++ src/routes/photoRoutes.ts
@@ -1,11 +1,10 @@
 import express from "express";
 import { uploadPhoto, getPalette } from "../controllers/photoController";
 import { authMiddleware } from "../middleware/authMiddleware";
-import upload from "../middleware/upload";
 
 const router = express.Router();
 
-router.post("/upload", authMiddleware as any, upload.single("image"), uploadPhoto);
+router.post("/upload", authMiddleware as any, uploadPhoto);
 router.get("/:photoId/palette", authMiddleware as any, getPalette);
 
 export default router;
