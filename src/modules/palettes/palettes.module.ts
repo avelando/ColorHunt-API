7774f218d74998f432e589c75866af2a6aee88e3
avelando <avelarrodrigues89@gmail.com>
2025-03-17 02:02:56 -0300

    +1

--- src/modules/palettes/palettes.module.ts
+++ src/modules/palettes/palettes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PalettesController } from './palettes.controller';
 import { PalettesService } from './palettes.service';
+import { CloudinaryModule } from 'src/config/cloudinary.module';
 
 @Module({
+  imports: [CloudinaryModule],
   controllers: [PalettesController],
   providers: [PalettesService]
 })
