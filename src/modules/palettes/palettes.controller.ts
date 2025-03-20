62e29f45b249a6af586e978664556a8be41a4237
avelando <avelarrodrigues89@gmail.com>
2025-03-20 14:14:48 -0300

    Correção na rota estática

--- src/modules/palettes/palettes.controller.ts
+++ src/modules/palettes/palettes.controller.ts
@@ -43,43 +43,6 @@ export class PalettesController {
     return this.palettesService.createPaletteWithImage(file, dto);
   }
 
-  @Get('user')
-  @ApiOperation({ summary: 'Obter paletas do usuário logado' })
-  async getUserPalettes(@Req() req: CustomRequest) {
-    if (!req.user || !req.user.id) {
-      throw new HttpException('User not authenticated', HttpStatus.UNAUTHORIZED);
-    }
-    return this.palettesService.getUserPalettes(req.user.id);
-  }
-
-  @Get(':paletteId')
-  @ApiOperation({ summary: 'Obter paleta pelo ID' })
-  async getPalette(@Param('paletteId') paletteId: string) {
-    return this.palettesService.getPalette(paletteId);
-  }
-
-  @Patch(':paletteId')
-  @ApiOperation({ summary: 'Atualizar paleta' })
-  async updatePalette(
-    @Req() req: CustomRequest,
-    @Param('paletteId') paletteId: string,
-    @Body() dto: UpdatePaletteDto,
-  ) {
-    if (!req.user || !req.user.id) {
-      throw new HttpException('User not authenticated', HttpStatus.UNAUTHORIZED);
-    }
-    return this.palettesService.updatePalette(paletteId, dto);
-  }
-
-  @Delete(':paletteId')
-  @ApiOperation({ summary: 'Excluir paleta' })
-  async deletePalette(@Req() req: CustomRequest, @Param('paletteId') paletteId: string) {
-    if (!req.user || !req.user.id) {
-      throw new HttpException('User not authenticated', HttpStatus.UNAUTHORIZED);
-    }
-    return this.palettesService.deletePalette(paletteId);
-  }
-
   @Get('public/user')
   @ApiOperation({ summary: 'Obter paletas públicas do usuário logado' })
   async getUserPublicPalettes(@Req() req: CustomRequest) {
@@ -100,13 +63,50 @@ export class PalettesController {
   
     return await this.palettesService.getExplorePalettes(req.user.id);
   }
-    
+
+  @Get('user')
+  @ApiOperation({ summary: 'Obter paletas do usuário logado' })
+  async getUserPalettes(@Req() req: CustomRequest) {
+    if (!req.user || !req.user.id) {
+      throw new HttpException('User not authenticated', HttpStatus.UNAUTHORIZED);
+    }
+    return this.palettesService.getUserPalettes(req.user.id);
+  }
+
   @Get('details/:paletteId')
   @ApiOperation({ summary: 'Obter detalhes da paleta' })
   async getPaletteDetails(@Param('paletteId') paletteId: string) {
     return this.palettesService.getPaletteDetails(paletteId);
   }
 
+  @Get(':paletteId')
+  @ApiOperation({ summary: 'Obter paleta pelo ID' })
+  async getPalette(@Param('paletteId') paletteId: string) {
+    return this.palettesService.getPalette(paletteId);
+  }
+
+  @Delete(':paletteId')
+  @ApiOperation({ summary: 'Excluir paleta' })
+  async deletePalette(@Req() req: CustomRequest, @Param('paletteId') paletteId: string) {
+    if (!req.user || !req.user.id) {
+      throw new HttpException('User not authenticated', HttpStatus.UNAUTHORIZED);
+    }
+    return this.palettesService.deletePalette(paletteId);
+  }
+
+  @Patch(':paletteId')
+  @ApiOperation({ summary: 'Atualizar paleta' })
+  async updatePalette(
+    @Req() req: CustomRequest,
+    @Param('paletteId') paletteId: string,
+    @Body() dto: UpdatePaletteDto,
+  ) {
+    if (!req.user || !req.user.id) {
+      throw new HttpException('User not authenticated', HttpStatus.UNAUTHORIZED);
+    }
+    return this.palettesService.updatePalette(paletteId, dto);
+  }
+  
   @Post(':paletteId/duplicate')
   @ApiOperation({ summary: 'Duplicar uma paleta' })
   async duplicatePalette(
