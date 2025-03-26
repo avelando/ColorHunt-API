c8ddca3562680772102a8a5a31e216da79acb965
avelando <avelarrodrigues89@gmail.com>
2025-03-26 00:47:36 -0300

    Adicionando prefixo

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,8 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('colorhunt');
+
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3000;
 
